9b584605ea431311e1f808b483496b790e8e92e5
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-02-05 19:41:59 +0000

    Attempt bug fix for initial deployment

--- fudge_kitchen/settings.py
+++ fudge_kitchen/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://roos-fudge-kitchen-9f7897dcad9e.herokuapp.com/', '8000-ajmccredie-fudgekitchen-g7huh7ai6bl.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['roos-fudge-kitchen-9f7897dcad9e.herokuapp.com/', '8000-ajmccredie-fudgekitchen-g7huh7ai6bl.ws-eu108.gitpod.io']
 
 
 # Application definition
