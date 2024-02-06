1a1d892df15a4fc5f40aeff0798901f14078616b
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-02-06 14:58:05 +0000

    Attempt bug fix for deployment

--- fudge_kitchen/settings.py
+++ fudge_kitchen/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['.herokuapp.com', '8000-ajmccredie-fudgekitchen-g7huh7ai6bl.ws-eu108.gitpod.io']
 
