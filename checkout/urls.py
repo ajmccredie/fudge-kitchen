a7e05fa9f56b1b7388fe772c58b4863a623e1013
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-02-13 20:22:31 +0000

    Add access keys to Stripe for webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import CheckoutView, CheckoutSuccessView
+from .webhooks import webhook
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('checkout/success/<order_number>/', CheckoutSuccessView.as_view(), name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
