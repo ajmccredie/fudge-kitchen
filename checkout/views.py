ad73383a9463dab6e32791254f241b67fa454ca8
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-02-13 12:07:56 +0000

    Fix subtotal bug on basket. Unresolved products bug on checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,17 +1,40 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.views import View
+from django.conf import settings
 
+import stripe
+
+from basket.contexts import basket_contents
 from .forms import OrderForm
 
 class CheckoutView(View):
     def get(self, request, *args, **kwargs):
-        basket = request.session.get('basket', {})
-        if not basket:
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+        basket_items = basket_contents(request)
+        if not basket_items:
             messages.error(request, "There's nothing in your basket at the moment")
             return redirect(reverse('product_list'))
 
+        current_basket = basket_contents(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        print(intent)
+
         order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
--- fudge_kitchen/settings.py
+++ fudge_kitchen/settings.py
@@ -208,6 +208,11 @@ if 'USE_AWS' in os.environ:
 
 DEFAULT_DELIVERY_CHARGE = 3
 
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
