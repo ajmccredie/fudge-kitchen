d4317234b129f04a820e988e2058814088b41179
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-05-09 10:59:37 +0000

    Add forms and update template and views to start to sort allergen filter function

---
+++ edible_products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class AllergenFilterForm(forms.Form):
+    ALLERGEN_CHOICES = [
+        ('gluten', 'Gluten'),
+        ('crustaceans', 'Crustaceans'),
+        ('eggs', 'Eggs'),
+        ('fish', 'Fish'),
+        ('peanuts', 'Peanuts'),
+        ('soybeans', 'Soybeans'),
+        ('milk', 'Milk'),
+        ('nuts', 'Nuts'),
+        ('celery', 'Celery'),
+        ('mustard', 'Mustard'),
+        ('sesame_seeds', 'Sesame Seeds'),
+        ('sulphur_dioxide_and_sulphites', 'Sulphur Dioxide and Sulphites'),
+        ('lupin', 'Lupin'),
+        ('molluscs', 'Molluscs'),
+    ]
+    allergens = forms.MultipleChoiceField(
+        choices=ALLERGEN_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Exclude Allergens:"
+    )
--- edible_products/views.py
+++ edible_products/views.py
@@ -1,42 +1,60 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView, View, TemplateView
 from django.http import JsonResponse
 from django.contrib import messages
 from decimal import Decimal, InvalidOperation
 from django.db.models import Q
 from .models import EdibleProduct, ProductWeightPrice, Allergen
+from .forms import AllergenFilterForm
 
 class EdibleProductListView(ListView):
+    # model = EdibleProduct
+    # context_object_name = 'edible_products'
+    # template_name = 'edible_products/product_list.html'
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     query = self.request.GET.get('q')
+    #     allergen_ids = self.request.GET.getlist('allergen[]')  
+    #     print(allergen_ids)
+
+    #     if query:
+    #         queryset = queryset.filter(
+    #             Q(flavour__icontains=query) |  # Search in flavour
+    #             Q(details__icontains=query) |  # Search in details
+    #             Q(ingredients__icontains=query)  # Search in ingredients
+    #         )
+
+    #     # Filter out products with the specified allergens
+    #     if allergen_ids:
+    #         for allergen_id in allergen_ids:
+    #             queryset = queryset.exclude(**{f"{allergen_id}": True})
+
+    #     return queryset
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['search_query'] = self.request.GET.get('q', '')
+    #     context['allergens'] = Allergen.objects.all() 
+    #     context['allergen_filters'] = self.request.GET.getlist('allergen[]')
+    #     return context
+
     model = EdibleProduct
-    context_object_name = 'edible_products'
     template_name = 'edible_products/product_list.html'
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        query = self.request.GET.get('q')
-        allergen_ids = self.request.GET.getlist('allergen[]')  
-        print(allergen_ids)
-
-        if query:
-            queryset = queryset.filter(
-                Q(flavour__icontains=query) |  # Search in flavour
-                Q(details__icontains=query) |  # Search in details
-                Q(ingredients__icontains=query)  # Search in ingredients
-            )
-
-        # Filter out products with the specified allergens
-        if allergen_ids:
-            for allergen_id in allergen_ids:
-                queryset = queryset.exclude(**{f"{allergen_id}": True})
-
+        if 'allergens' in self.request.GET:
+            allergens = self.request.GET.getlist('allergens')
+            for allergen in allergens:
+                queryset = queryset.exclude(**{allergen: True})
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['search_query'] = self.request.GET.get('q', '')
-        context['allergens'] = Allergen.objects.all() 
-        context['allergen_filters'] = self.request.GET.getlist('allergen[]')
+        context['allergen_form'] = AllergenFilterForm(self.request.GET)
         return context
+
 
 class EdibleProductDetailView(DetailView):
     model = EdibleProduct
@@ -54,4 +72,3 @@ class GetPriceView(View):
             return JsonResponse({'price': str(weight_price_obj.price)})
         except (ValueError, EdibleProduct.DoesNotExist, ProductWeightPrice.DoesNotExist, InvalidOperation):
             return JsonResponse({'error': 'Invalid product ID or weight'}, status=400)
-
