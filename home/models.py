f703305d5edd680b0faab07fb4a6101fdef2abd0
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-05-17 10:35:52 +0000

    Updated where images in home are uploaded to

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class OurStory(models.Model):
     title = models.CharField(max_length=200, help_text="Enter the page title")
     content = models.TextField(help_text="Enter the page content")
-    image = models.ImageField(upload_to='our_story_images/', null=True, blank=True, help_text="Upload an image for the story")
+    image = models.ImageField(null=True, blank=True, help_text="Upload an image for the story")
     last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
