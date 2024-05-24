cf840fc890a3f17a1c55db806e9894c3c91f32a7
aj mccredie <aj.mccredie@yahoo.co.uk>
2024-05-24 21:41:59 +0000

    Add search functionality across the site

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import FAQsView
+from .views import FAQsView, SearchResultsView
 
 app_name = 'home'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('our_story/', views.our_story, name='our_story'),
     path('faqs/', FAQsView.as_view(), name='faqs'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,12 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .forms import InquiryForm
+from django.views import View
+from django.db.models import Q
+from core.models import CommonProduct
+from edible_products.models import EdibleProduct
+from merch.models import MerchProduct
+from profiles.models import SubscriptionProduct
 
 # Create your views here.
 
@@ -39,3 +45,38 @@ class FAQsView(TemplateView):
             messages.success(request, 'Thank you for messaging our admin')
             return redirect('home:faqs')
         return self.render_to_response(self.get_context_data(form=form))
+
+
+class SearchResultsView(View):
+    template_name = 'home/search_results.html'
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        if query:
+            # Searching edible products
+            edible_products = EdibleProduct.objects.filter(
+                Q(name__icontains=query) | 
+                Q(flavour__icontains=query)
+            )
+
+            # Searching merch products
+            merch_products = MerchProduct.objects.filter(
+                Q(name__icontains=query) | 
+                Q(description__icontains=query)
+            )
+
+            # Searching subscription products
+            subscription_products = SubscriptionProduct.objects.filter(
+                Q(name__icontains=query) | 
+                Q(description__icontains=query)
+            )
+        else:
+            edible_products = merch_products = subscription_products = None
+
+        context = {
+            'edible_products': edible_products,
+            'merch_products': merch_products,
+            'subscription_products': subscription_products,
+            'query': query
+        }
+        return render(request, self.template_name, context)
